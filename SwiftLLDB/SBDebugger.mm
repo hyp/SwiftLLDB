1c91d9288c7c169abfa1c03e2f91a6e81b3c2e18
Alex Lorenz <arphaman@gmail.com>
2015-12-10 20:30:44 +0000

    Fix typo (found by @mattrajca)

--- SwiftLLDB/SBDebugger.mm
+++ SwiftLLDB/SBDebugger.mm
@@ -35,7 +35,7 @@
 }
 
 /// A helper method that returns a pointer to a C FILE for that file handle.
-FILE *openFileHanle(NSFileHandle *fileHandle, const char *mode, NSError **error) {
+FILE *openFileHandle(NSFileHandle *fileHandle, const char *mode, NSError **error) {
     FILE *inputFile = fdopen([fileHandle fileDescriptor], mode);
     if (!inputFile) {
         if (error) {
@@ -50,21 +50,21 @@ FILE *openFileHanle(NSFileHandle *fileHandle, const char *mode, NSError **error)
 }
 
 - (BOOL) setInputFileHandle: (nonnull NSFileHandle *)fileHandle error:(NSError **)error {
-    FILE *f = openFileHanle(fileHandle, "r", error);
+    FILE *f = openFileHandle(fileHandle, "r", error);
     if (!f) { return NO; }
     debugger.SetInputFileHandle(f, /*transfer_ownership=*/false);
     return YES;
 }
 
 - (BOOL) setOutputFileHandle: (nonnull NSFileHandle *)fileHandle error:(NSError **)error {
-    FILE *f = openFileHanle(fileHandle, "w", error);
+    FILE *f = openFileHandle(fileHandle, "w", error);
     if (!f) { return NO; }
     debugger.SetOutputFileHandle(f, /*transfer_ownership=*/false);
     return YES;
 }
 
 - (BOOL) setErrorFileHandle: (nonnull NSFileHandle *)fileHandle error:(NSError **)error {
-    FILE *f = openFileHanle(fileHandle, "w", error);
+    FILE *f = openFileHandle(fileHandle, "w", error);
     if (!f) { return NO; }
     debugger.SetErrorFileHandle(f, /*transfer_ownership=*/false);
     return YES;
